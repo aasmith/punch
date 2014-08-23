ffe1482ed2b8b8a7d609baebfe4206f79b4fdd56
Andrew A Smith <andy@tinnedfruit.org>
2014-08-23 11:14:48 -0700

    Adding various initialization options

--- lib/punch.rb
+++ lib/punch.rb
@@ -1,3 +1,29 @@
+require "mechanize"
+
 class Punch
   VERSION = "1.0.0"
+
+  HOST = "adpeet.adp.com"
+  URL  = "https://%s/%s/applications/wpk/html/kronos-logonbody.jsp"
+
+  attr_reader :url
+
+  # Initializes Punch with either a client ID or a URL. An optional
+  # host can also be specified. If a URL is provided, all other args
+  # are ignored.
+
+  def initialize(client_id: nil, host: HOST, url: nil)
+    fail "Need client_id or url" unless client_id or url
+
+    self.url = url || URL % [host || HOST, client_id]
+
+    @agent = Mechanize.new do |a|
+      a.ssl_version = :TLSv1
+    end
+  end
+
+  def url=(url)
+    @url = URI.parse url
+  end
+
 end
--- test/test_punch.rb
+++ test/test_punch.rb
@@ -1,8 +1,30 @@
 require "minitest/autorun"
 require "punch"
 
-class TestPunch < MiniTest::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+class TestPunch < Minitest::Test
+
+  def test_init_with_url
+    p = Punch.new url: "http://derp"
+
+    assert_equal p.url.to_s, "http://derp"
   end
+
+  def test_init_with_client_id
+    p = Punch.new client_id: "123"
+
+    assert_equal p.url.to_s, Punch::URL % [Punch::HOST, "123"]
+  end
+
+  def test_init_with_client_id_and_host
+    p = Punch.new client_id: "123", host: "derp"
+
+    assert_equal p.url.to_s, Punch::URL % ["derp", "123"]
+  end
+
+  def test_init_with_nothing
+    assert_raises RuntimeError do
+      Punch.new
+    end
+  end
+
 end
