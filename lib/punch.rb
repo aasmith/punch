48dd4855513ee3a5862ae9d6330920cc22cfed6f
Andrew A Smith <andy@tinnedfruit.org>
2014-08-24 17:36:23 -0700

    Now displays and saves a timecard

--- lib/punch.rb
+++ lib/punch.rb
@@ -1,10 +1,14 @@
+require "cgi"
+require "date"
+require "uri"
+
 require "mechanize"
 
 class Punch
   VERSION = "1.0.0"
 
   HOST = "adpeet.adp.com"
-  URL  = "https://%s/%s/applications/wpk/html/kronos-logonbody.jsp&ESS=true"
+  URL  = "https://%s/%s/applications/wpk/html/kronos-logonbody.jsp?ESS=true"
 
   attr_reader :url
 
@@ -20,8 +24,6 @@ class Punch
     @agent = Mechanize.new do |a|
       a.ssl_version = :TLSv1
     end
-
-    @page = nil
   end
 
   def url=(url)
@@ -37,15 +39,196 @@ class Punch
       f["authenticateWithSecurityQuestion"] = false
     end
 
-    @page = form.submit
+    form.submit
   end
 
-  def fetch_timecard
-    p @page
+  def fetch_timecard(home_page)
+    nav = home_page.iframe("contentPane").click
+    Timecard.new nav.link_with(text: "My Timecard").click
+  end
 
-    timecard = @page.link_with(:text => 'My Timecard').click
+  # A wrapper around a Mechanize::Page that contains a timecard form.
+  #
+  # Used to mutate forms so they can be saved or approved.
+  #
+  # A Timecard contains two forms:
+  #
+  #   * The display form which starts as the initial state of the time card.
+  #     This form is mutated in memory to represent any changes the user
+  #     wants to make to the time card.
+  #
+  #   * The submission form which distills the display form into a compact
+  #     version for server submission.
 
-    p timecard
+  class Timecard
+    ACTIONS = { save: "Save", approve: "approve" }
+
+    attr_accessor :page
+
+    def initialize(page)
+      self.page = page
+    end
+
+    def display_form
+      page.form("pageDisplayForm")
+    end
+
+    alias form display_form
+
+    def employee_id
+      form["employeeId"]
+    end
+
+    def start_date
+      Date.strptime(form["beginTimeframeDate"], "%m/%d/%Y")
+    end
+
+    def end_date
+      Date.strptime(form["endTimeframeDate"], "%m/%d/%Y")
+    end
+
+    def xfer_fields
+      form.fields.select do |field|
+        field.name =~ /^T\d+R\d+C3$/
+      end
+    end
+
+    # Populate the timecard. It seems eTime cards for FTEs are already
+    # filled out with hours, so only the xfer code is required.
+    def populate(xfer:)
+      xfer_fields.each do |field|
+        field.value = xfer
+      end
+    end
+
+    def submission_form
+      page.form("com.kronos.wfc.command")
+    end
+
+    def prepare_submission(action: :save)
+      unless ACTIONS.keys.include? action
+        fail "action must be one of :approve or :save"
+      end
+
+      populate_employee_ids employee_id
+      populate_action action
+      populate_data
+    end
+
+    def populate_fields(field_names:, value:, form: submission_form)
+      [*field_names].each do |field_name|
+        field = form.field(field_name)
+        field.value = value
+      end
+    end
+
+    def populate_employee_ids(employee_id)
+      populate_fields(
+        field_names: %w(employeeId employeeIds personIds),
+        value: employee_id
+      )
+    end
+
+    def populate_action(action)
+      populate_fields(
+        field_names: "com.kronos.wfc.ACTION",
+        value: ACTIONS.fetch(action)
+      )
+    end
+
+    def populate_data
+      populate_fields(
+        field_names: "com.kronos.wfc.CMD_DATA",
+        value: serialize_fields
+      )
+    end
+
+    def serialize_fields
+      out = []
+
+      out << xfer_fields.map do |xfer_field|
+        [xfer_field.name, CGI.escape(xfer_field.value)].join("=")
+      end
+
+      # We could've used CGI::encode_www_form if it wasn't for this:
+      out.join(",")
+    end
+
+    def save
+      prepare_submission
+      submission_form.submit
+    end
+
+    def suggest_save_or_approve(todays_date = Date.today)
+      todays_date > end_date ? :approve : :save
+    end
+
+    def pretty
+      out = []
+
+      items = form.fields.select do |field|
+        field.name =~ /T\d+R\d+C\d+/
+      end
+
+      out << "Timecard for Employee %s (%s-%s)" % [
+        employee_id,
+        start_date,
+        end_date
+      ]
+
+      current_date = start_date
+      current_week = nil
+
+      days_or_depts = items.sort_by { |x| x.name.scan(/\d+/).map(&:to_i) }
+
+      days_or_depts.each do |day_or_dept|
+        day_or_dept.name =~ /T(\d+)R\d+C(\d+)/ or raise "Unknown day/dept"
+
+        week, day = $1.to_i, $2.to_i
+
+        if current_week != week
+          out << ""
+          out << "Week #{week}"
+          out << "------"
+          current_week = week
+        end
+
+        case day
+        when 2     then out << format_other("Dept", day_or_dept.value)
+        when 3     then out << format_other("Xfer", day_or_dept.value)
+        when 4..10 then
+          out << format_day(day, current_date, day_or_dept.value)
+          current_date += 1
+        end
+      end
+
+      out.join("\n")
+    end
+
+    def format_other(title, value)
+      "%s: %s" % [title, value.empty? ? "-" : value]
+    end
+
+    def format_day(day, date, hours)
+      unless (day - 4) == date.wday
+        fail "Sanity check failed, days of week do not match"
+      end
+
+      "%s %s: %s" % [
+        Date::ABBR_DAYNAMES[day - 4], date, hours.empty? ? "None" : hours
+      ]
+    end
+
+    def marshal_dump
+      [page.response, page.body]
+    end
+
+    def marshal_load(values)
+      response, body = values
+
+      @page = Mechanize::Page.new nil, response, body, nil, Mechanize.new
+    end
+
   end
 
 end
--- test/test_punch.rb
+++ test/test_punch.rb
@@ -27,4 +27,62 @@ class TestPunch < Minitest::Test
     end
   end
 
+  def test_populate_timecard_with_xfer
+    load_example_timecard
+
+    @timecard.populate xfer: "/derp//"
+
+    @timecard.xfer_fields.each do |field|
+      assert_equal "/derp//", field.value
+    end
+  end
+
+  def test_timecard_prints
+    load_example_timecard
+
+    expected = File.read "test/expected_tcard.txt"
+
+    assert_equal expected, @timecard.pretty
+  end
+
+  def test_timecard_prepare_submission
+    load_example_timecard
+
+    @timecard.populate xfer: "/derp//"
+    @timecard.prepare_submission
+
+    form = @timecard.submission_form
+
+    %w(employeeId employeeIds personIds).each do |employee_field_name|
+      field = form.field_with(name: employee_field_name)
+
+      assert_equal @timecard.employee_id, field.value
+    end
+
+    action = form.field_with(name: "com.kronos.wfc.ACTION")
+    assert_equal "Save", action.value
+
+    serialized_fields = "T0R0C3=%2Fderp%2F%2F,T1R0C3=%2Fderp%2F%2F,\
+                         T2R0C3=%2Fderp%2F%2F,T3R0C3=%2Fderp%2F%2F".delete(" ")
+
+    cmd = form.field_with(name: "com.kronos.wfc.CMD_DATA")
+    assert_equal serialized_fields, cmd.value
+  end
+
+  def test_suggest_save_or_approve
+    load_example_timecard
+
+    too_soon   = @timecard.end_date - 1
+    too_eager  = @timecard.end_date
+    just_right = @timecard.end_date + 1
+
+    assert_equal :save,    @timecard.suggest_save_or_approve(too_soon)
+    assert_equal :save,    @timecard.suggest_save_or_approve(too_eager)
+    assert_equal :approve, @timecard.suggest_save_or_approve(just_right)
+  end
+
+  def load_example_timecard
+    @timecard = Marshal.load File.open("test/tcard.dump")
+  end
+
 end
